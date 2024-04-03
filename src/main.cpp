1638ee39946eb7811ae501ab88aa897b9cea5122
Josef Melcr <melcrjos@fit.cvut.cz>
2024-04-03 14:06:28 +0200

    redo main

--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,26 @@
 int main(int argc, char * argv[]) {
     bool ret_val = false;
 
-    if (argc > 1) {
-        std::ifstream ifs(argv[1]);
-        if (!ifs) {
-            std::cerr << "\033[31m" << "[ERROR] " << "\033[m" << "Couldn't open file: " << argv[1] << std::endl;
-            return 1;
-        }
-        Parser parser(ifs);
-        ret_val = parser.Parse();
-        parser.Generate().print(llvm::outs(), nullptr);
-    } else {
-        Parser parser;
-        ret_val = parser.Parse();
+    std::string infile = argc > 1 ? argv[1] : "";
+    std::ifstream ifs(infile);
+
+    std::istream & in = argc > 1 ? ifs : std::cin;
+
+    if (argc > 1 && !ifs) {
+            std::cerr << "\033[31m"
+                      << "[ERROR] "
+                      << "\033[m"
+                      << "Couldn't open file: " << argv[1] << std::endl;
+            return EXIT_FAILURE;
     }
+
+    Parser parser(in);
+    ret_val = parser.Parse();
 
     if (!ret_val) {
         return EXIT_FAILURE;
     }
+    parser.Generate().print(llvm::outs(), nullptr);
 
     return EXIT_SUCCESS;
 }
